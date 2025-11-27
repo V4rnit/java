cfd25fdba9fbb57262be06b9d350b75741283eaa
V4rnit <121328381+V4rnit@users.noreply.github.com>
2025-11-27 05:02:04 +0000

    Level order traversal for a binary tree (BFS)

---
+++ trees/levelOrderTraversal.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class levelOrderTraversal {
+    public static void bfs(TreeNode root){
+        if(root == null){
+            return;
+        }
+        Queue <TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll();
+            System.out.println(curr.val);
+
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+            if(curr.right != null){
+                q.add(curr.right);
+            }
+        }
+    }
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(20);
+        root.left = new TreeNode(10);
+        root.right = new TreeNode(30);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(15);
+        root.right.left = new TreeNode(25);
+        root.right.right = new TreeNode(40);
+
+        System.out.println("The level order traversal for the tree is: ");
+
+        bfs(root);
+    }
+}
