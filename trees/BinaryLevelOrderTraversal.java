2648870569ae25a3012dcca637814cf2c2f93990
V4rnit <rawatvarnit122@gmail.com>
2025-12-06 01:56:27 -0500

    Practice

---
+++ trees/BinaryLevelOrderTraversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int val){
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class BinaryLevelOrderTraversal{
+
+	public static List<List<Integer>> levelOrder(TreeNode root){
+		
+	}
+
+	public static void main(String args[]){
+		
+	}
+}
