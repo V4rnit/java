390190265ad77aa67597db22a191e4c49ee81575
V4rnit <rawatvarnit122@gmail.com>
2025-12-10 15:46:28 -0500

    Solved the problem of rotating an array left by one using Brute and Optimal approach

--- arrays/leftRotate.java
+++ arrays/leftRotate.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 class leftRotate{
-	public static int [] leftRotate(int [] arr){
+	public static int [] leftRotateBrute(int [] arr){
 		int [] res = new int [arr.length];
+		
 		for(int i = 1; i < arr.length; i++){
 			res[i-1] = arr[i];
 		}
@@ -10,15 +11,35 @@ class leftRotate{
 
 		return res;
 	}
+
+	public static void  leftRotateOptimal(int[] arr){
+		int t = arr[0];
+		for(int i = 1; i < arr.length; i++){
+			arr[i-1] = arr[i];
+		}
+		arr[arr.length - 1] = t;
+
+	}
 	public static void main(String args[]){
-		int [] array = {1,2,3,4,5,6,7,8,9,0};
-		int [] res = leftRotate(array);
-		
-		for(int i = 0; i < res.length; i++ ){
-			System.out.print(res[i] + " ");
+		//Using Brute Force approach here
+		int [] bruteArray = {1,2,3,4,5,6,7,8,9,0};
+
+		int [] resBrute = leftRotateBrute(bruteArray);
+
+		for(int i = 0; i < resBrute.length; i++){
+			System.out.print(resBrute[i] +" ");
 		}
 		System.out.println();
+
+		//Using Optimal Approach here
+		int [] optimalArray = {3,4,5,6,7,8,9,0};
+		leftRotateOptimal(optimalArray);
+
+		for(int i = 0; i < optimalArray.length; i++){
+			System.out.print(optimalArray[i] + " ");
+		}
+		System.out.println();
+		
 	}
 }
-
 
