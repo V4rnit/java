9626e7c44d9a902f3c3393cd6d18bf86889481c9
V4rnit <rawatvarnit122@gmail.com>
2025-12-14 19:29:39 -0500

    Finding if the given Number is prime or not

---
+++ Methods/findPrime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class findPrime{
+	public static boolean isPrime(int n){
+		if( n < 2 ){
+			return false;
+		}
+		for(int i = 2; i < n/2 ; i++){
+			if(n % i == 0) return false;
+		}
+		return true;
+	}
+	public static void main(String args[]){
+		int number = 99;
+
+		System.out.println(isPrime(number));
+	}
+}
+
