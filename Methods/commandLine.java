ca7ba3ad689961e68b6d7c9e596c51deb3347665
V4rnit <rawatvarnit122@gmail.com>
2025-12-15 18:51:31 -0500

    Taking command line arguments from user

---
+++ Methods/commandLine.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class commandLine{
+	public static void main(String [] args){
+		double sum = 0;
+
+		for(String s:args){
+			sum = sum + Double.parseDouble(s);
+		}
+		System.out.println(sum);
+	}
+}
