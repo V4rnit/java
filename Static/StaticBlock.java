41d3ce4d294eb15a9d5f1c7366017eb9cdcc3924
V4rnit <rawatvarnit122@gmail.com>
2025-12-25 00:00:19 -0500

    Practicing Static Blocks

---
+++ Static/StaticBlock.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test {
+	static{
+		System.out.println("Block 1");
+	}
+	static {
+		System.out.println("Block 2");
+	}
+}
+public class StaticBlock{
+	public static void main(String [] args){
+		System.out.println("Main");
+		Test t = new Test();
+	}
+}
