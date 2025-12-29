d91d3738fd2a7d3d1772b0f5e6ae5a3e2c7c65d2
V4rnit <rawatvarnit122@gmail.com>
2025-12-29 03:42:24 -0500

    Learning how to create my own Package in Java and use it for my program

---
+++ Packages/learnPackage.java
@@ -0,0 +1,13 @@
+import mypack1.Demo;
+import mypack1.Demo2;
+import  mypack1.inner.Demo3;
+public class learnPackage{
+	public static void main(String [] args){
+		Demo d1 = new Demo();
+		d1.display();
+		Demo2 d2 = new Demo2();
+		d2.display();
+		Demo3 d3 = new Demo3();
+		d3.display();
+	}
+}
