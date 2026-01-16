153b6506fe08ac5a84fb111c51ff8208fd8de55e
V4rnit <rawatvarnit122@gmail.com>
2026-01-16 12:34:01 -0500

    Solved leetcode 19 (Medium)

--- LinkedList/RemoveNode.java
+++ LinkedList/RemoveNode.java
@@ -18,21 +18,20 @@ class ListNode{
 }
 class Solution{
 	public ListNode removeNthFromEnd(ListNode head, int n){
-		ListNode dummy = new ListNode(0, head);
+		ListNode dummy = new ListNode(-1, head);
 		ListNode left = dummy;
 		ListNode right = head;
 		while(n > 0){
 			right = right.next;
 			n--;
 		}
-
 		while(right != null){
-			right = right.next;
 			left = left.next;
+			right = right.next;
 		}
-
 		left.next = left.next.next;
 		return dummy.next;
+
 	}
 }
 public class RemoveNode{
