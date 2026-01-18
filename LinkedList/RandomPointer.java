1ee17f79954c5859c20824198535afcff4a8aa66
V4rnit <rawatvarnit122@gmail.com>
2026-01-18 18:35:23 -0500

    Solved leetcode 138

---
+++ LinkedList/RandomPointer.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Node{
+	public int val;
+	public Node next;
+	public Node random;
+
+	//Constructor
+	Node(int val){
+		this.val = val;
+		this.next = null;
+		this.random = null;
+	}
+}
+class Solution{
+	public Node copyRandomList(Node head){
+		if(head == null){
+			return head;
+		}
+		HashMap <Node, Node> map = new HashMap<>();
+		Node curr = head;
+		
+		while(curr != null){
+			map.put(curr, new Node(curr.val));
+			curr = curr.next;
+		}
+		curr = head;
+		while(curr != null){
+			map.get(curr).next = map.get(curr.next);
+			map.get(curr).random = map.get(curr.random);
+			curr = curr.next;
+		}
+
+		return map.get(head);
+	}
+}
+public class RandomPointer{
+	public static void main(String [] args){
+
+	}
+}
