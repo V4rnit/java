b0b7e336a4f6e95f06f80c836e37cc5bdee5499f
V4rnit <rawatvarnit122@gmail.com>
2026-01-20 13:23:52 -0500

    Solved leetcode Valid Parenthesis problem

--- Stack/ValidParenthesis.java
+++ Stack/ValidParenthesis.java
@@ -1,24 +1,23 @@
 import java.util.*;
 class Solution{
 	public boolean isValid(String s){
-		Stack<Character> valid = new Stack<>();
+		Stack <Character> stack = new Stack<>();
 		for(char c : s.toCharArray()){
 			if(c == '(' || c == '{' || c == '[') {
-				valid.push(c);
+				stack.push(c);
 			}
 			else{
-				if(valid.isEmpty()) return false;
-				char top = valid.pop();
+				if(stack.isEmpty()) return false;
+				char top = stack.pop();
 				if(
-				(c == ')' && top != '(') ||
-				(c == ']' && top != '[') ||
-				(c == '}') && top != '{' ) {
+					(c == ')' && top != '(') ||
+					(c == '}' && top != '{') ||
+					(c == ']' && top != '[')){
 					return false;
 					}
-
 			}
 		}
-		return valid.isEmpty();
+		return stack.isEmpty();
 	}
 }
 public class ValidParenthesis{
@@ -30,6 +29,6 @@ public class ValidParenthesis{
 		Solution sol = new Solution();
         	System.out.println(sol.isValid(test1)); // true
         	System.out.println(sol.isValid(test2)); // false
-        	System.out.println(sol.isValid(test3));
+        	System.out.println(sol.isValid(test3)); //true
 	}
 }
