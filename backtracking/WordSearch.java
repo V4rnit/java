a9562eb5354f60aa270b1f0fa46abb9738449df1
V4rnit <rawatvarnit122@gmail.com>
2026-01-31 18:38:51 -0500

    Solving Leetcode 79 as taught in class cs2 (Medium)

--- backtracking/WordSearch.java
+++ backtracking/WordSearch.java
@@ -4,32 +4,31 @@ public class WordSearch{
 	public static boolean search(char [][] board, String word){
 		int r = board.length;
 		int c = board[0].length;
-		
+		boolean [][] visited = new boolean[r][c];
 		for(int i = 0; i < r; i++)		{
 			for(int j = 0; j < c; j++){
-				if(dfs(board, word, i, j, 0)){
+				if(dfs(board, word, i, j, 0, visited)){
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	public static  boolean dfs(char [][] board, String word, int row, int col, int idx ){
+	public static  boolean dfs(char [][] board, String word, int row, int col, int idx, boolean[][] visited ){
 		if(idx == word.length()){
 			return true;
 		}
-		if(row < 0 || col < 0 || row >= board.length || col >= board[0].length 
-				|| board[row][col] != word.charAt(idx)){
-			return false;
-				}
-		char temp = board[row][col];
-		board[row][col] = '#';
-		boolean found = dfs(board, word, row + 1, col, idx + 1)||
-				dfs(board, word, row - 1, col, idx + 1 ) ||
-				dfs(board, word, row, col + 1, idx + 1)  ||
-				dfs(board, word, row, col -1, idx + 1);
-		board [row][col] = temp;
-		return found;
+		if(row >= 0 && col >= 0 && row < board.length && col < board[0].length && board[row][col] == word.charAt(idx) && visited[row][col] == false){
+			visited[row][col] = true;
+
+			if(dfs(board, word, row + 1, col, idx + 1, visited)) return true;
+			if(dfs(board, word, row, col + 1, idx + 1, visited)) return true;
+			if(dfs(board, word, row - 1, col, idx + 1, visited)) return true;
+			if(dfs(board, word, row, col - 1, idx + 1, visited)) return true;
+
+			visited[row][col] = false;
+		}
+		return false;
 	}
 	public static void main(String [] args){
 		char[][] board1 = {
