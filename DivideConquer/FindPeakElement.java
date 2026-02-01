51a1c0e8cff93f8c9567d3220f50df94760efe95
V4rnit <rawatvarnit122@gmail.com>
2026-02-01 16:55:05 -0500

    Solved leetcode Finding peak in an array

--- DivideConquer/FindPeakElement.java
+++ DivideConquer/FindPeakElement.java
@@ -1,27 +1,23 @@
 class Solution{
 	public int findPeakElement(int [] nums){
-		int left = 0;
-		int right = nums.length - 1;
-		
-		while(left < right){
-			int mid = left + (right - left) / 2;
+		int l = 0;
+		int r = nums.length - 1;
+		while(l < r){
+			int mid = l + (r - l) / 2;
 			if(nums[mid] < nums[mid + 1]){
-				left = mid + 1;
+				l = mid + 1;
 			}
-			//The inverse is nums[mid] >= nums[mid + 1]
-			//If this is the case right = mid because right might be the peak itself
 			else{
-				right = mid;
+				r = mid;
 			}
 		}
-		return left;
+		return l;
 	}
 }
 public class FindPeakElement{
 	public static void main(String [] args){
 		Solution sol = new Solution();
 		int [] nums = {1, 2, 3, 1};
-		//out should be 2
 		System.out.println(sol.findPeakElement(nums));
 	}
 }
