59a1e06fc0d789fc5a717428b4e6c9c3c9ee6991
V4rnit <rawatvarnit122@gmail.com>
2026-02-04 20:56:36 -0500

    Leetcode 226 (Easy) also implemented BFS to show before and after state of tree

--- trees/invert.java
+++ trees/invert.java
@@ -11,38 +11,51 @@ class TreeNode{
         this.right = null;
     }
 }
-public class invert {
-    public static TreeNode invertBinaryTree(TreeNode head){
-        if(head == null){
-            return null;
-        }
-        TreeNode temp = head.left;
-        head.left = head.right;
-        head.right = temp;
+class Solution{
+	public TreeNode invertTree(TreeNode root){
+		if(root == null){
+			return null;
+		}
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
 
-        invertBinaryTree(head.left);
-        invertBinaryTree(head.right);
-        return head;
-    }
-    public static void bfs(TreeNode root){
-        if(root == null){
-            return;
-        }
-        Queue <TreeNode> q = new LinkedList<>();
-        q.add(root);
+		invertTree(root.left);
+		invertTree(root.right);
+		return root;
+	}
+	public List<List<Integer>> levelOrder(TreeNode root){
+		List<List<Integer>> res = new ArrayList<>();
+		if(root == null){
+			return res;
+		}
+		Queue <TreeNode> q = new LinkedList<>();
+		q.offer(root);
 
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            System.out.println(curr.val);
-            if(curr.left != null){
-                q.add(curr.left);
-            }
-            if(curr.right != null){
-                q.add(curr.right);
-            }
-        }
-    }
+		while(!q.isEmpty()){
+			int lvl = q.size();
+			List<Integer> currLvl = new ArrayList<>();
+
+			for(int i = 0; i < lvl; i++){
+				TreeNode currNode = q.poll();
+				currLvl.add(currNode.val);
+
+				if(currNode.left != null){
+					q.offer(currNode.left);
+				}
+				if(currNode.right != null){
+					q.offer(currNode.right);
+				}
+			}
+			res.add(currLvl);
+		}
+		return res;
+	}
+}
+public class invert{
     public static void main(String args[]) {
+	Solution sol = new Solution();
+
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
@@ -50,12 +63,13 @@ public class invert {
         root.left.right = new TreeNode(5);
         root.right.right = new TreeNode(6);
         root.right.left = new TreeNode(12);
+	
+ 	List<List<Integer>> before = sol.levelOrder(root);
+	System.out.println(before);
 
-        System.out.println("Before inverting the tree");
-        bfs(root);
-
-        System.out.println("After inverting the tree");
-        invertBinaryTree(root);
-        bfs(root);
+	TreeNode inverted = sol.invertTree(root);
+	List<List<Integer>> after = sol.levelOrder(inverted);
+	System.out.println(after);
+	
     }
 }
