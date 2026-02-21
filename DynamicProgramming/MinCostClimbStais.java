062c4e56318114e2cdcd957bd804b0967bc78c70
V4rnit <rawatvarnit122@gmail.com>
2026-02-20 23:21:35 -0500

    Solving this problem with Botton Up DP with & without space optimization

--- DynamicProgramming/MinCostClimbStais.java
+++ DynamicProgramming/MinCostClimbStais.java
@@ -1,6 +1,6 @@
 class Solution{
 	//Bottom Up Solution with no space optimization
-	public int minCost(int [] cost){
+	public int minCostBetter(int [] cost){
 		//if(cost.length <= 2){
 		//	return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
 		//}
@@ -14,6 +14,22 @@ class Solution{
 		}
 		return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
 	}
+
+	//Bottom Up Solution with space optimization
+	public int minCostOptimal(int [] cost){
+		if(cost.length <= 2){
+			return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+		}
+		int prev1 = cost[1];
+		int prev2 = cost[0];
+
+		for(int i = 2; i < cost.length; i++){
+			int currCost = cost[i] + Math.min(prev1, prev2);
+			prev2 = prev1;
+			prev1 = currCost;
+		}
+		return Math.min(prev1, prev2);
+	}
 }
 public class MinCostClimbStais{
 	public static void main(String[] args){
@@ -21,6 +37,7 @@ public class MinCostClimbStais{
 		//Out should be 6
 		int [] cost = {1,100,1,1,1,100,1,1,100,1};
 
-		System.out.println(sol.minCost(cost));
+		System.out.println(sol.minCostBetter(cost));
+		System.out.println(sol.minCostOptimal(cost));
 	}
 }
