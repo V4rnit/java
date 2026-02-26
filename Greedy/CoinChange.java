e5f4979ebdd6ae7f6b365b6f64819eb52151df3b
V4rnit <rawatvarnit122@gmail.com>
2026-02-26 14:44:26 -0500

    Coing Change using Greedy (USA) only

--- Greedy/CoinChange.java
+++ Greedy/CoinChange.java
@@ -1,15 +1,17 @@
 class Solution{
 	public int minChange(int n){
-		int quarters = n / 25;
-		int remainingAfterQuarter = n % 25;
-		
-		int dime = remainingAfterQuarter / 10;
-		int remainingAfterDime = remainingAfterQuarter % 10;
-		
-		int nickle = remainingAfterDime / 5;
-		int remainingAfterNickle = remainingAfterDime % 5;
-		
-		return (quarters + dime + nickle + remainingAfterNickle);
+		int quarter = n / 25;
+		int remainAfterQuarter = n % 25;
+
+		int dime = remainAfterQuarter / 10;
+		int remainAfterDime = n % 10;
+
+		int nickle = remainAfterDime / 5;
+		int remainAfterNickle = n % 5;
+
+		int pennies = remainAfterNickle;
+
+		return (quarter + dime + nickle + pennies);
 	}
 }
 public class CoinChange{
